b6ff0a3d40346cc7b8910cc3608f6faa044ea1f5
ganout <morgan.bourdillel@gmail.com>
2020-04-29 16:20:44 +0200

    add main controller and repository

--- feeltheforce/src/main/java/com/joda/feeltheforce/controller/MainController.java
+++ feeltheforce/src/main/java/com/joda/feeltheforce/controller/MainController.java
@@ -1,18 +1,35 @@
 package com.joda.feeltheforce.controller;
 
 import com.joda.feeltheforce.entities.People;
+import com.joda.feeltheforce.entities.Planet;
+import com.joda.feeltheforce.repository.PeopleRepository;
+import com.joda.feeltheforce.repository.PlanetRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class MainController {
 
-    @GetMapping("/match")
+    @GetMapping("/")
     public String match(Model model) {
 
-        return "/";
+        PlanetRepository planetRepository = new PlanetRepository();
+        List<Planet> planets = planetRepository.findAll();
+
+        model.addAttribute(planets);
+
+        PeopleRepository peopleRepository = new PeopleRepository();
+        List<People> peoples = new ArrayList<>();
+
+        model.addAttribute(peoples);
+
+        return "index";
     }
+
 
     @GetMapping("/about-us")
     public String aboutUs() {

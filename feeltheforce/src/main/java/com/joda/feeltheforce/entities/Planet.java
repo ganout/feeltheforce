01c2ae34053bebf1aa182f52dee5c4e574ffdcf4
ganout <morgan.bourdillel@gmail.com>
2020-04-29 13:46:40 +0200

    add a new entity : planet

---
+++ feeltheforce/src/main/java/com/joda/feeltheforce/entities/Planet.java
@@ -0,0 +1,97 @@
+public class Planet {
+
+    private int id;
+    private String name;
+    private int rotation_period;
+    private int orbital_period;
+    private int diameter;
+    private String climate;
+    private String gravity;
+    private String terrain;
+    private String surface_water;
+    private long population;
+
+    public Planet() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRotation_period() {
+        return rotation_period;
+    }
+
+    public void setRotation_period(int rotation_period) {
+        this.rotation_period = rotation_period;
+    }
+
+    public int getOrbital_period() {
+        return orbital_period;
+    }
+
+    public void setOrbital_period(int orbital_period) {
+        this.orbital_period = orbital_period;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(int diameter) {
+        this.diameter = diameter;
+    }
+
+    public String getClimate() {
+        return climate;
+    }
+
+    public void setClimate(String climate) {
+        this.climate = climate;
+    }
+
+    public String getGravity() {
+        return gravity;
+    }
+
+    public void setGravity(String gravity) {
+        this.gravity = gravity;
+    }
+
+    public String getTerrain() {
+        return terrain;
+    }
+
+    public void setTerrain(String terrain) {
+        this.terrain = terrain;
+    }
+
+    public String getSurface_water() {
+        return surface_water;
+    }
+
+    public void setSurface_water(String surface_water) {
+        this.surface_water = surface_water;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(long population) {
+        this.population = population;
+    }
+}

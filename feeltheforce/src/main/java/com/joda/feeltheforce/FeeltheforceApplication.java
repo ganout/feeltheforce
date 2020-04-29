12a1770635496e1654f8ffdb5d7e854a141d8363
mroldan747 <mroldan747@gmail.com>
2020-04-29 10:34:31 +0200

    init

---
+++ feeltheforce/src/main/java/com/joda/feeltheforce/FeeltheforceApplication.java
@@ -0,0 +1,13 @@
+package com.joda.feeltheforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeltheforceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeeltheforceApplication.class, args);
+	}
+
+}
---
+++ feeltheforce/src/test/java/com/joda/feeltheforce/FeeltheforceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joda.feeltheforce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeeltheforceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

bf00006788c7e44bafd4b2a626f9cea69cc8cf92
ganout <morgan.bourdillel@gmail.com>
2020-04-30 09:31:44 +0200

    fix for position red circle

--- feeltheforce/src/main/resources/static/lightsaber.js
+++ feeltheforce/src/main/resources/static/lightsaber.js
@@ -2,5 +2,5 @@ const progressBar = document.getElementsByClassName('lightsaber')[0];
 setInterval(() => {
     const computedStyle = getComputedStyle(progressBar);
     const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0;
-    progressBar.style.setProperty('--width', width + .1)
+    progressBar.style.setProperty('--width', width + .2)
 }, 5)

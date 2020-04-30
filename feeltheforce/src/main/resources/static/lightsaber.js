e1769e472bbbf1352b87a49004a2cd5f5c22d456
mroldan747 <mroldan747@gmail.com>
2020-04-30 13:25:32 +0200

    not sound

--- feeltheforce/src/main/resources/static/lightsaber.js
+++ feeltheforce/src/main/resources/static/lightsaber.js
@@ -1,6 +1,9 @@
-const progressBar = document.getElementsByClassName('lightsaber')[0];
-setInterval(() => {
-    const computedStyle = getComputedStyle(progressBar);
-    const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0;
-    progressBar.style.setProperty('--width', width + .2)
-}, 5)
+document.addEventListener('DOMContentLoaded', function() {
+    const progressBar = document.getElementsByClassName('lightsaber')[0];
+    setInterval(() => {
+        const computedStyle = getComputedStyle(progressBar);
+        const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0;
+        progressBar.style.setProperty('--width', width + .2);
+    }, 5)
+
+});
